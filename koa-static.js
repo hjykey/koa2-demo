47b1dc5d02cf58647aca4d8094f16e0ef05154c4
song <276959848@qq.com>
2020-11-04 17:45:20 +0800

    update

--- koa-static.js
+++ koa-static.js
@@ -1,17 +1,19 @@
 //安装中间件npm install --save koa-static
-const Koa = require("koa");
-const path = require("path");
-const static = require("koa-static");
+const Koa = require('koa')
+const path = require('path')
+const static = require('koa-static')
 
-const app = new Koa();
-const staticPath = "./static";
+const app = new Koa()
+const staticPath = './static'
 
-app.use(static(path.join(__dirname, staticPath)));
+app.use(static(path.join(__dirname, staticPath)))
 
 app.use(async (ctx) => {
-  ctx.body = "hello world";
-});
+  ctx.body = '输入/资源'
+  // ctx.response.type = 'text'
+  // ctx.response.body = fs.createReadStream('text.txt')
+})
 
 app.listen(3000, () => {
-  console.log("[demo] static-use-middleware is starting at port 3000");
-});
+  console.log('[demo] static-use-middleware is starting at port 3000')
+})
